346857302a01fef9d83311c663e39ce8fe0e6b3c
kryptiksage <kryptiksage@gmail.com>
2020-08-17 10:45:30 +0530

    main.rs : added redirect

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,26 @@
 
 #[macro_use] extern crate rocket;
 
+use rocket::response::Redirect;
+mod utils;
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello world"
 }
 
 #[get("/search?<cmd>")]
-fn search(cmd: String) -> &'static str {
+fn search(cmd: String) -> Redirect {
     println!("cmd: {}", cmd);
-    "Hello from the search page"
+    let command = utils::get_cmd_from_query(&cmd);
+    let redirect_url = match command {
+        "tw" => String::from("https://twitter.com"),
+        "gl" => String::from("https://gitlab.com"),
+        _ => String::from("https://google.com")
+    };        
+    Redirect::to(redirect_url)
 }
 
 fn main() {
     rocket::ignite().mount("/", routes![index, search]).launch();
 }
-
-fn get_cmd_from_query(query: &str) -> &str {
-    let test = "hello";
-    return &test;
-}
-
